56a74a8fac1a14c75e012b23490ae4d6d290fa8f
Daniel P Corson <danl@alum.mit.edu>
2017-05-26 13:28:09 -0400

    fix database locked errors and make notifier pop up correctly

--- src/NotifOmg.hs
+++ src/NotifOmg.hs
@@ -5,9 +5,11 @@ import Control.Concurrent.Chan
 import Control.Monad
 import Data.List
 import Data.Maybe
+import System.Directory
 import System.Exit
 import System.FilePath
 import System.IO
+import System.Posix
 import System.Process
 import qualified AnsiColor as C
 
@@ -19,48 +21,55 @@ memo chan pOut = forever $ hGetLine pOut >>= writeChan chan . Just
 
 pop :: FilePath -> Chan (Maybe String) -> IO ()
 pop home chan = do
-  h <- openFile (home </> ".memoplex" </> "seen") ReadWriteMode
-  forever $ hGetLine h >> writeChan chan Nothing
+    let seenF = home </> ".config" </> "memoplex" </> "seen"
+    doesFileExist seenF >>= flip when (removeFile seenF)
+    createNamedPipe seenF $ unionFileModes ownerReadMode ownerWriteMode
+    -- Must be ReadWriteMode even though we don't read due to strangeness
+    -- with named pipes. Using a low level blocking openFile didn't work.
+    -- But this does.
+    h <- openFile seenF ReadWriteMode
+    forever $ hGetLine h >> writeChan chan Nothing
 
 getC :: [Char] -> [Char]
 getC a = head . (++ [C.yellow]) . map snd $ filter (($ a) . fst) getCFs
 
 getCFs :: [([Char] -> Bool, [Char])]
-getCFs = [
-  (("Mail:gen:" `isPrefixOf`), C.cyan),
-  (("Mail:goo:" `isPrefixOf`), C.magenta),
-  (("aim:" `isPrefixOf`), C.white)
-  ]
+getCFs =
+    [ (("Mail:gen:" `isPrefixOf`), C.cyan)
+    , (("Mail:goo:" `isPrefixOf`), C.magenta)
+    , (("aim:" `isPrefixOf`), C.white)
+    ]
 
 windowAct :: Handle -> Bool -> Maybe String -> IO Bool
 windowAct pIn alreadyMarked Nothing = do
-  hPutStrLn pIn ""
-  hFlush pIn
-  unless alreadyMarked . putStrLn $ C.green ++ "-----------------------------"
-  system "wmctrl -r NOTIFIEROMG -b add,below || true"
-  return True
+    hPutStrLn pIn ""
+    hFlush pIn
+    unless alreadyMarked . putStrLn $
+        C.green ++ "-----------------------------"
+    system "wmctrl -r NOTIFIEROMG -b add,below || true"
+    return True
 windowAct _ _ (Just a) = do
-  putStrLn $ getC a ++ a
-  system "wmctrl -r NOTIFIEROMG -b add,above || true"
-  return False
+    putStrLn $ getC a ++ a
+    system "wmctrl -r NOTIFIEROMG -b add,above || true"
+    return False
 
 handler :: ThreadId -> ProcessHandle -> IO ()
 handler mainThreadId pId = do
-  terminateProcess pId
-  throwTo mainThreadId (ExitFailure 1)
+    terminateProcess pId
+    throwTo mainThreadId (ExitFailure 1)
 
 notifOmg :: String -> IO ()
 notifOmg home = do
-  hSetBuffering stdout LineBuffering
-  chan <- newChan
-  mainThreadId <- myThreadId
-  -- using a separate process is historical;
-  -- can switch to in-process?
-  (pIn, pOut, pErr, pId) <-
-    runInteractiveProcess "memoplex" ["-r"] Nothing Nothing
-  installHandler sigINT (Catch $ handler mainThreadId pId) Nothing
-  hSetBuffering pOut LineBuffering
-  forkIO $ memo chan pOut
-  forkIO $ pop home chan
-  c <- getChanContents chan
-  foldM_ (windowAct pIn) False c
+    hSetBuffering stdout LineBuffering
+    chan <- newChan
+    mainThreadId <- myThreadId
+    -- using a separate process is historical;
+    -- can switch to in-process?
+    (pIn, pOut, pErr, pId) <-
+      runInteractiveProcess "memoplex" ["-r"] Nothing Nothing
+    installHandler sigINT (Catch $ handler mainThreadId pId) Nothing
+    hSetBuffering pOut LineBuffering
+    forkIO $ memo chan pOut
+    forkIO $ pop home chan
+    c <- getChanContents chan
+    foldM_ (windowAct pIn) False c
--- src/Opts.hs
+++ src/Opts.hs
@@ -4,36 +4,37 @@ import System.Console.GetOpt
 
 data MemoMode = MemoRead | MemoWrite | MemoServe | MemoNotifOmg
 
-data Options = Options {
-  optHelp :: Bool,
-  optMode :: Maybe MemoMode,
-  optPullHosts :: [String],
-  optShowIds :: Bool
-  }
+data Options = Options
+    { optHelp :: Bool
+    , optMode :: Maybe MemoMode
+    , optPullHosts :: [String]
+    , optShowIds :: Bool
+    }
 
 defOpts :: Options
-defOpts = Options {
-  optHelp = False,
-  optMode = Nothing,
-  optPullHosts = [],
-  optShowIds = False
-  }
+defOpts = Options
+    { optHelp = False
+    , optMode = Nothing
+    , optPullHosts = []
+    , optShowIds = False
+    }
 
 options :: [OptDescr (Options -> Options)]
-options = [
-  Option "h" ["help"] (NoArg (\ o -> o {optHelp = True})) "",
-  Option "r" ["read"] (NoArg (\ o -> o {optMode = Just MemoRead}))
-    "Read messages as they appear.",
-  Option "s" ["serve"] (NoArg (\ o -> o {optMode = Just MemoServe}))
-    "Connect writes to reads.",
-  Option "w" ["write"] (NoArg (\ o -> o {optMode = Just MemoWrite}))
-    "Accept new messages on stdin.",
-  Option "N" ["notifieromg"] (NoArg (\ o -> o {optMode = Just MemoNotifOmg}))
-    "Connect to custom terminal-based notifier.",
-  Option "p" ["pull-from-host"] 
-    (ReqArg (\ s o -> o {optPullHosts = s : optPullHosts o}) "HOST")
-    "Host to also pull memos from. (multiple -p possible)",
-  Option "i" ["show-ids"] (NoArg (\ o -> o {optShowIds = True}))
-    "Only useful for memoplex communication between servers."
-  ]
+options =
+    [ Option "h" ["help"] (NoArg (\ o -> o {optHelp = True})) ""
+    , Option "r" ["read"] (NoArg (\ o -> o {optMode = Just MemoRead}))
+      "Read messages as they appear."
+    , Option "s" ["serve"] (NoArg (\ o -> o {optMode = Just MemoServe}))
+      "Connect writes to reads."
+    , Option "w" ["write"] (NoArg (\ o -> o {optMode = Just MemoWrite}))
+      "Accept new messages on stdin."
+    , Option "N" ["notifieromg"]
+      (NoArg (\ o -> o {optMode = Just MemoNotifOmg}))
+      "Connect to custom terminal-based notifier."
+    , Option "p" ["pull-from-host"] 
+      (ReqArg (\ s o -> o {optPullHosts = s : optPullHosts o}) "HOST")
+      "Host to also pull memos from. (multiple -p possible)"
+    , Option "i" ["show-ids"] (NoArg (\ o -> o {optShowIds = True}))
+      "Only useful for memoplex communication between servers."
+    ]
 
